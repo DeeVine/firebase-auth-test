56671ea079bdb2854d83ca7e1ab377289e1531fb
Darren Kwong <darrenkwong@Darrens-MacBook-Pro.local>
2017-09-03 22:26:07 -0700

    updates

---
+++ assets/app.js
@@ -0,0 +1,83 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCqBmBb_wf5kqabmHatj844pI4D3S_6O9s",
+  authDomain: "fir-auth-755f9.firebaseapp.com",
+  databaseURL: "https://fir-auth-755f9.firebaseio.com",
+  projectId: "fir-auth-755f9",
+  storageBucket: "fir-auth-755f9.appspot.com",
+  messagingSenderId: "688777003307"
+};
+firebase.initializeApp(config);
+
+// FirebaseUI config.
+var uiConfig = {
+  signInSuccessUrl: 'https://deevine.github.io/firebase-auth-test/',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>'
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+initApp = function() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var uid = user.uid;
+      var phoneNumber = user.phoneNumber;
+      var providerData = user.providerData;
+      user.getIdToken().then(function(accessToken) {
+        document.getElementById('sign-in-status').textContent = 'Signed in';
+        document.getElementById('sign-in').textContent = 'Sign out';
+        document.getElementById('account-details').textContent = JSON.stringify({
+          displayName: displayName,
+          email: email,
+          emailVerified: emailVerified,
+          phoneNumber: phoneNumber,
+          photoURL: photoURL,
+          uid: uid,
+          accessToken: accessToken,
+          providerData: providerData
+        }, null, '  ');
+      });
+      //add sign out function
+
+      $('#sign-in').on("click", function(){
+        event.preventDefault();
+        console.log("testing sign out button");
+        
+        firebase.auth().signOut().then(function() {
+          // Sign-out successful.
+        }).catch(function(error) {
+          // An error happened.
+        });
+      });
+    } else {
+      // User is signed out.
+      document.getElementById('sign-in-status').textContent = 'Signed out';
+      document.getElementById('sign-in').textContent = 'Sign in';
+      document.getElementById('account-details').textContent = 'null';
+    }
+  }, function(error) {
+    console.log(error);
+  });
+};
+
+window.addEventListener('load', function() {
+  initApp()
+});
